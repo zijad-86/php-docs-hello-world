8cd68b07af81777c913b041506005bdca92b9f91
zijad-86 <134355764+zijad-86@users.noreply.github.com>
2024-03-23 14:07:53 +0100

    JsonFormating

--- index.php
+++ index.php
@@ -6,6 +6,9 @@ $apidata = "https://d192b65.myviolet.de/getBackofficeData"; // Replace with your
 
 $response = file_get_contents($apidata);
 
+// format the Json data from $response
+$response = json_decode($response, true);
+
 // Handle the response as needed
 echo $response;
 
